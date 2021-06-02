051efb691658204327dc6d2da10d324c93fa84ee
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-06-02 11:39:52 +0100

    header added to print statement

--- lib/print_statement.rb
+++ lib/print_statement.rb
@@ -3,4 +3,9 @@ require_relative 'account'
 
 class PrintStatment
 
+
+  def header
+    "date       || credit       || debit       || balance\n"
+  end
+  
 end
--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -1,5 +1,7 @@
 require 'print_statement'
 
 describe PrintStatment do
-  
+  it 'can print the statement header' do
+    expect(subject.header).to eq("date       || credit       || debit       || balance\n")
+  end
 end
